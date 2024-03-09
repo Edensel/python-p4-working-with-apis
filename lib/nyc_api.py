e940219be1ecb5c971e15770ccb6141d0b08d0f8
Densel <densel.esekon@moringaschool.com>
2024-03-09 10:50:57 +0300

    Completed

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -4,11 +4,23 @@ import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        response = requests.get(URL)
+        return response.json()
 
-    response = requests.get(URL)
-    return response.content
+    def program_school(self):
+        programs_list = []
+        programs = self.get_programs()
+        for program in programs:
+            program_data = json.loads(program)
+            programs_list.append(program_data["agency"])
+        return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+programs = GetPrograms()
+programs_schools = programs.program_school()
+
+for school in set(programs_schools):
+    print(school)
+
+
